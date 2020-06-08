a5c2a07070253fe2f9aed2f3da342461acb1979f
Kenn Mutai <mutaikenn@gmail.com>
2020-06-08 16:35:29 +0300

    Setting up useEffect hooks

--- server/index.js
+++ server/index.js
@@ -4,8 +4,26 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+//Connection and disconnection through the
+io.on("connection", (socket) => {
+  console.log("There is a new connection");
+
+  socket.on("join", ({ name, room }) => {
+    console.log(name, room);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user has left");
+  });
+});
+
+// Route middleware
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
